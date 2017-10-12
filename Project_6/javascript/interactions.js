565422595a11dd746641d51000adb6e01383f095
Joseph Emelike <juemelike@gmail.com>
2017-10-12 00:16:28 -0400

    Beginning of Project 6

---
+++ Project_6/javascript/interactions.js
@@ -0,0 +1,4 @@
+var user = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+$(document).ready(
+    user.forEach($("#streams").append("<div id='" + user + "'>" + user[i] + "</div>")));
