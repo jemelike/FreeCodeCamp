8680d14decb34cb8377c36b33fa263c4e37e7f41
Joseph Emelike <juemelike@gmail.com>
2017-10-13 01:50:07 -0400

    Project_6 | displayUser() function is functional

--- Project_6/javascript/interaction.js
+++ Project_6/javascript/interaction.js
@@ -1,11 +1,13 @@
 var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var container;
 
-$("document").ready(
+function displayUser() {
+    container = document.getElementById('container');
     users.forEach(function(user) {
         console.log(user);
-        $.append(user);
+        console.log(container);
+        container.innerHTML += "<div id=" + user + "class =''>" + user + "</div>";
     })
-);
+}
 
-/*
-}*/
+$("document").ready(displayUser);
