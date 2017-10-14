14cdd89f0b80d807fc122a0433613f0c00366858
Joseph Emelike <juemelike@gmail.com>
2017-10-14 14:31:48 -0400

    Test angular project

--- Project_6/javascript/interaction.js
+++ Project_6/javascript/interaction.js
@@ -1,12 +1,28 @@
 var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 var container;
 
+function online(stream) {
+    if (stream == null)
+        return "offline";
+    else
+        return "online";
+}
+
 function displayUser() {
     container = document.getElementById('user_list');
     users.forEach(function(user) {
-        console.log(user);
-        console.log(container);
-        container.innerHTML += "<li id=" + user + "class ='row_item'>" + user + "</li>";
+        $.getJSON("https://wind-bow.glitch.me/twitch-api/streams/" + user + "/").done(
+            function(data) {
+                console.log(" user: " + user + " value:" + data.stream);
+                var isOnline = online(data.stream);
+                container.innerHTML += "<li id='" + user + "'class ='item'>" + user + " <p class='status " + isOnline + "'>" + isOnline + "</p></li>";
+                if (isOnline == "online") {
+                    var temp = document.getElementById(user);
+                    console.log("temp: " + temp + " user: " + user);
+                    temp.innerHTML += "<p>Game: " + data.stream.game + " Viewer: " + data.stream.viewers + "Mature" + data.channel + "</p>";
+                }
+            }
+        );
     })
 }
 
