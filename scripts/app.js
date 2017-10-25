3dd81f76a1eeeb8e8d6830e36ea72b08d8f62f63
Joseph Emelike <juemelike@gmail.com>
2017-10-25 18:18:46 -0400

    Completed the indepth search

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,26 @@
-var myRepo = "https://api.github.com/repos/jemelike/FreeCodeCamp/contents";
+var myRepoContents = "https://api.github.com/repos/jemelike/FreeCodeCamp/contents";
 angular.module("MyApp", ["ngMaterial"])
 
 .controller("test", function() {
-    $.getJSON(myRepo).done(function(data) {
-        console.log(data);
+    console.log("marker");
+    //Retrieve project subfolders using GitHub API and displays the contents of the ReadME file
+    $.getJSON(myRepoContents).done(function(data) {
+        $(data).each(function(i) {
+            if (data[i]['name'].includes('Project')) {
+                console.log(data[i]['name']);
+                $.getJSON(myRepoContents + "/" + data[i]['name'] + "/" + "README.md")
+                    .done(function(data) {
+                        $.getJSON(data['git_url'])
+                            .done(function(data) {
+                                var README = atob(data['content']);
+
+                                console.log(README);
+                            });
+                    });
+            }
+
+        })
+
     });
 
 })
